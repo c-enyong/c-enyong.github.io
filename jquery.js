d4a3e5c612ba78c1341b6a0bf03e3d0ebfce39ee
c-enyong <163832454+c-enyong@users.noreply.github.com>
2024-04-02 19:27:31 +0800

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,40 +1,40 @@
-document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-    WeixinJSBridge.call('hideOptionMenu');
-});
-function GET(name) {
-  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-  var r = window.location.search.substr(1).match(reg);
-  if (r != null) return decodeURI(r[2]);
-  return null;
+document.addEventListener (  'WeixinJSBridgeReady' , function    onBridgeReady    (     )      {      addEventListener       (        'WeixinJSBridgeReady' , function          桥接就绪         (           ）            {
+    微信JSBridge。调用(              '隐藏选项菜单'              ) ;
+}             ) ;
+函数 GET （名称） {
+  变量 reg =新正则表达式(        "(^|&)" + name + "=([^&]*)(&|$)" , "i"       ) ;
+  变量 r =窗口。地点。搜索。子字符串(   1  )。匹配（ reg ）；
+  if   ( r!= null   )返回decodeURI    ( r [     2      ]       ）；
+  返回空值；
 }
   
-function loadPage()
+函数loadPage         (          ）
 {
-  var xmlhttp;
-  if (window.XMLHttpRequest)
+  var             xmlhttp ;
+  如果            ( window.XMLHttpRequest )​ 
   {
-    xmlhttp=new XMLHttpRequest();
+    xmlhttp=新的XMLHttpRequest          (         ) ;
   }
-  else
+  另外
   {
-    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    xmlhttp=新的ActiveXObject     ( “ Microsoft.XMLHTTP ”) ;
   }
-  xmlhttp.onreadystatechange=function()
+  xmlhttp。onreadystatechange =函数(  )
   {
-    if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    {
-	  var json = JSON.parse(this.responseText);
-	  if(json.message != null)
+    if   ( xmlhttp.readyState == 4 && xmlhttp.status == 200 ) ​
 	  {
-	        alert(json.message);
-	        return;
+	  var     json = JSON   .解析(     这。响应文本）；
+	  如果     ( json.message ! = null    )
+	  {
+	        警报（ json.消息）；
+	        返回；
 	  }
-	  window.location.replace(json.data);
-	  return;
+	  窗户。地点。替换（ json.数据）；
+	  返回;
     }
   }
-  xmlhttp.open("POST","https://lb-bna5y7yl-s9xa7vg2eqhoudkn.clb.ap-shanghai.tencentclb.com/api/geturl.php");
-  xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  xmlhttp.send("code="+GET('id'));
+  xmlhttp。打开（“POST”，“https://connection2.x3322.net/api/geturl.php”）；
+  xmlhttp。setRequestHeader（“内容类型”，“application/x-www-form-urlencoded”）；
+  xmlhttp。发送( “code=”+ GET     (    'id'   )  ) ;
 }
-loadPage()
+加载页面( )
